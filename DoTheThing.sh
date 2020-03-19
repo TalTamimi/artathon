0b3b682a5720da02330adaabda63ddbfa6964dd6
Ahmed Saleh Alhamed <asalhamed@gmail.com>
2020-03-19 15:37:15 +0300

    Update DoTheThing.sh

--- DoTheThing.sh
+++ DoTheThing.sh
@@ -3,6 +3,6 @@ for STYLE_FILE in Style/*.png; do
 	mkdir "Out/$out_folder";
 	for INPUT_FILE in Content/*.jpg; do
 		echo $out_folder;
-	       curl `curl -F "content=@${INPUT_FILE}" -F "style=@${STYLE_FILE}" -H 'api-key:b070a6b8-ee49-4342-a833-1c9f050e653f' https://api.deepai.org/api/fast-style-transfer | jq -j '.output_url'` -o "Out/$out_folder/$(basename -- $INPUT_FILE)"
+	       curl `curl -F "content=@${INPUT_FILE}" -F "style=@${STYLE_FILE}" -H 'api-key:0000000000000000000000000000' https://api.deepai.org/api/fast-style-transfer | jq -j '.output_url'` -o "Out/$out_folder/$(basename -- $INPUT_FILE)"
 	done
 done
